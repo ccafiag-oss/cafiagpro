a80834019d0b43bdede55e6a08c6adbd9d00b58a
CAFIAG <ccafiag@gmail.com>
2026-09-04 18:00:03 +0000

    Correction de la connexion PostgreSQL avec process.env.DATABASE_URL

--- src/routesgstock/backup.js
+++ src/routesgstock/backup.js
@@ -1,9 +1,2 @@
-const express = require('express');
-const router = express.Router();
-const pool = require('../config/db');
-const { exec } = require('child_process');
-const cron = require('node-cron');
-const path = require('path');
-const fs = require('fs');
 
 
